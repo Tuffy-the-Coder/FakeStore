8d4b57e479c9407a9c812927f71a769990cf6480
Anurag <Rawat123anurag@gmail.com>
2026-02-24 04:32:40 +0530

    ProductPageUIupdated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -96,15 +96,17 @@ function Cart() {
     return (
         <>
             <div className=" gap-5 flex flex-col p-5 justify-evenly items-center">
-                <div className="bg-amber-50 w-full lg:w-60 lg:h-25 text-[0.54rem] lg:text-[1rem] rounded-[6px] lg:rounded-2xl flex flex-wrap gap-[2px] lg:gap-2 p-2 lg:p-3 fixed bottom-0 lg:right-4 lg:top-20">
+                
+                <div className="bg-amber-50 w-full lg:w-60 lg:h-25 text-[0.8rem] lg:text-[1rem] rounded-[6px] lg:rounded-2xl flex flex-wrap gap-[2px] lg:gap-2 p-2 lg:p-3 fixed bottom-0 lg:right-4 lg:top-20">
                     <p>Subtotal ({cartItems.length === 1 ? `${cartItems.length} item` : `${cartItems.length} items`}): </p>
                     <p className="font-bold">${subTotal.toFixed(2)}</p>
                     <Link to={`/Checkout/DeliveryDetails`} className="w-full">
-                    <button className="w-full h-5 lg:h-8 bg-orange-400 text-[0.6rem] lg:text-[1rem] rounded-2xl  font-['Calibri'] cursor-pointer hover:opacity-80 active:scale-90 active:opacity-60">
+                    <button className="w-full h-8 lg:h-8 bg-orange-400 text-[0.8rem] lg:text-[1rem] rounded-2xl  font-['Calibri'] cursor-pointer hover:opacity-80 active:scale-90 active:opacity-60">
                         Proceed to Buy
                     </button>
                     </Link>
                 </div>
+
                 {uniqueItems.map(item => (
                     <Link to={`/Item/${createSlug(item.title)}/${item.id}`} key={item.id}>
                         <div className="w-90 h-35 lg:w-140 lg:h-55 flex flex-row  overflow-hidden border-2 border-amber-300 rounded-2xl  hover:border-amber-500 cursor-pointer">
